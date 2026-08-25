ed7c4d26d820e025c9ec691ebcf704daae6bbb5c
João Vitor Hobus <69995779+miojo-dev@users.noreply.github.com>
2026-08-24 23:38:49 -0300

    feat/scratch of footer manager

---
+++ index.js
@@ -0,0 +1,17 @@
+let main, footer;
+
+debugger
+
+footer = `
+    <footer>
+        100% Handmade material - original creations only - João Vitor Hobus 2026 - Copyright - All rights reserved -
+    </footer>
+`;
+
+debugger
+
+main = document.getElementsByTagName("main");
+
+debugger
+
+main.after(footer);
