9a36a9b4e88660eac7e9a43a3762af35573285bc
João Vitor Hobus <69995779+miojo-dev@users.noreply.github.com>
2026-08-26 23:24:56 -0300

    feat/header and footer manager (#1)
    * feat/scratch of footer manager
    
    * feat/scratch of footer manager
    
    * feat/modularize header and footer

---
+++ index.js
@@ -0,0 +1,21 @@
+const year = new Date().getFullYear();
+const copyright = "100% Handmade material - original creations only - João Vitor Hobus " + year + " - Copyright - All rights reserved";
+
+const header = document.createElement("header")
+const main = document.querySelector("main");
+const footer = document.createElement("footer");
+
+header.innerHTML = `
+    <header>
+        <div>
+            <h1 class="title_wrapper">Underground pool</h1><span class="cursor">&nbsp;</span>
+        </div>
+    </header>
+`;
+
+footer.innerText = copyright;
+
+if (!document.querySelector("header")) {
+    main.before(header);
+}
+main.after(footer);
