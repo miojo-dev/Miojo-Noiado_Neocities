016ec655ecf871c745e1524ccae6bf2ac77ff74c
João Vitor Hobus <69995779+miojo-dev@users.noreply.github.com>
2026-08-31 23:03:37 -0300

    feat/alter playlists storage

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const year = new Date().getFullYear();
 const copyright = "100% Handmade material - original creations only - João Vitor Hobus " + year + " - Copyright - All rights reserved";
+const playlist_Catalog = [
+    "https://open.spotify.com/embed/playlist/74s4ZUJqpI9kdQ5QwnIfsJ?utm_source=generator&si=12b863a7540e4187",
+    "https://open.spotify.com/embed/playlist/1h4wSA4krYn4JnHd3H1Nmy?utm_source=generator&si=d6a826a1acac425f",
+    "https://open.spotify.com/embed/playlist/6elAVbJkQ2EADu3ilWCOdK?utm_source=generator&si=846d7f54978a4614",
+    "https://open.spotify.com/embed/playlist/7g3VB9m0LLdTbiLW6Ois3R?utm_source=generator&si=34b99495b7f34e09",
+    "https://open.spotify.com/embed/playlist/5D7l97n8apmZ0Amx82gMM0?utm_source=generator&si=9772ebae46fe4b86",
+    "https://open.spotify.com/embed/playlist/4LhBVK4zqeMhTY01AQTDtL?utm_source=generator&si=7113254b0ae6489f"
+];
 
 const header = document.createElement("header")
 const main = document.querySelector("main");
@@ -23,4 +31,12 @@ if (!document.querySelector("header")) {
 main.after(footer);
 
 let carrousel = document.querySelector(".carrousel");
+let carrousel_Numbering = document.querySelector(".carrousel-numbering")
 
+carrousel_Numbering.innerText = 1;
+
+function CarrouselHandler(direction) {
+    if (direction === "next") {
+
+    }
+}
